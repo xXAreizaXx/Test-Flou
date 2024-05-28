ca0b8729ddbe984e18c2ac88e6ab877d675a2d7c
Cesar Ortiz <cesarjavierortizmontero@gmail.co>
2024-05-28 11:12:57 -0500

    base proyect

---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+import Editor from '@/components/Editor';
+import '../styles/globals.css';
+
+export default function Home() {
+  return <Editor />;
+}
---
+++ src/components/plugins/CodeHighlightPlugin.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { registerCodeHighlighting } from '@lexical/code';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { useEffect } from 'react';
+
+const CodeHighlightPlugin: React.FC = () => {
+  const [editor] = useLexicalComposerContext();
+  useEffect(() => {
+    return registerCodeHighlighting(editor);
+  }, [editor]);
+  return null;
+};
+
+export default CodeHighlightPlugin;
